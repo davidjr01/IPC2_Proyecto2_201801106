1623c59090782b9c85032c9b875189cc690698f8
davidjr01 <davidljr01@gmail.com>
2021-06-30 15:26:03 -0600

    2.0

--- frontend/pag/views.py
+++ frontend/pag/views.py
@@ -10,6 +10,9 @@ from requests.models import encode_multipart_formdata
 endpoint='http://localhost:5000{}'
 
 lclientes=[]
+juegos=[]
+mclientes=[]
+juegosv=[]
 
 # Create your views here.
 def index(request):
@@ -26,39 +29,112 @@ def index(request):
         root = document.createElement('Documento')
 
         docs=request.FILES['document']
+        doc2=request.FILES['mclientes']
+        doc3=request.FILES['jv']
+        doc4=request.FILES['j']
         data=docs.read().decode('utf-8')
+        mc=doc2.read().decode('utf-8')
+        jv=doc3.read().decode('utf-8')
+        j=doc4.read().decode('utf-8')
+        #-----------
         linea=str(data)
         lc=linea.split('\n')
+        #-----------
+        linea2=str(mc)
+        lmc=linea2.split('\n')
+        #-----------
+        linea3=str(jv)
+        ljv=linea3.split('\n')
+        #-----------
+        linea4=str(j)
+        lj=linea4.split('\n')
+
         for linea in lc:
             linea=linea.strip('\r')
             lc2=linea.split(',')
             lclientes.append(lc2)
+        #-----------
+        for linea in lmc:
+            linea=linea.strip('\r')
+            lmc2=linea.split(',')
+            mclientes.append(lmc2)
+        #-----------
+        for linea in ljv:
+            linea=linea.strip('\r')
+            ljv2=linea.split(',')
+            juegosv.append(ljv2)
+        #-----------
+        for linea in lj:
+            linea=linea.strip('\r')
+            lj2=linea.split(',')
+            juegos.append(lj2)
+
 
         for linea in lclientes:
-            tarea_e = document.createElement('Cliente')
-            root.appendChild(tarea_e)
-            nombre = document.createElement('nombre')
+            t = document.createElement('Cliente')
+            root.appendChild(t)
+            nombre = document.createElement('Nombre')
             nombre.appendChild(document.createTextNode(linea[0]))
-            tarea_e.appendChild(nombre)
+            t.appendChild(nombre)
 
-            apellido = document.createElement('apellido')
+            apellido = document.createElement('Apellido')
             apellido.appendChild(document.createTextNode(linea[1]))
-            tarea_e.appendChild(apellido)
+            t.appendChild(apellido)
 
-            edad = document.createElement('edad')
+            edad = document.createElement('Edad')
             edad.appendChild(document.createTextNode(linea[2]))
-            tarea_e.appendChild(edad)
+            t.appendChild(edad)
 
-            cumpleaños = document.createElement('fecha_cumple_anos')
+            cumpleaños = document.createElement('Fecha_Cumple_Anos')
             cumpleaños.appendChild(document.createTextNode(linea[3]))
-            tarea_e.appendChild(cumpleaños)
+            t.appendChild(cumpleaños)
 
-            compra = document.createElement('primera_compra')
+            compra = document.createElement('Primera_Compra')
             compra.appendChild(document.createTextNode(linea[4]))
-            tarea_e.appendChild(compra)
+            t.appendChild(compra)
+        
+        #----------------------
+        for linea in mclientes:
+            t = document.createElement('Mejores_Clientes')
+            root.appendChild(t)
 
+            nombre = document.createElement('Nombre')
+            nombre.appendChild(document.createTextNode(linea[0]))
+            t.appendChild(nombre)
 
+            ucompra = document.createElement('Ultima_Compra')
+            ucompra.appendChild(document.createTextNode(linea[1]))
+            t.appendChild(ucompra)
 
+            cjv = document.createElement('Juegos_Vendidos')
+            cjv.appendChild(document.createTextNode(linea[2]))
+            t.appendChild(cjv)
+
+            cg = document.createElement('Cantidad_Gastada')
+            cg.appendChild(document.createTextNode(linea[3]))
+            t.appendChild(cg)
+
+        #----------------------------
+        for linea in jv:
+            t = document.createElement('Juegos_MasVendidos')
+            root.appendChild(t)
+
+            nombre = document.createElement('Nombre')
+            nombre.appendChild(document.createTextNode(linea[0]))
+            t.appendChild(nombre)
+
+            uc = document.createElement('Ultima_Compra')
+            uc.appendChild(document.createTextNode(linea[1]))
+            t.appendChild(uc)
+
+            cv = document.createElement('Copias_Vendidas')
+            cv.appendChild(document.createTextNode(linea[2]))
+            t.appendChild(cv)
+
+            st = document.createElement('Stoc')
+            st.appendChild(document.createTextNode(linea[3]))
+            t.appendChild(st)
+        
         xml = root.toprettyxml(indent='\t', encoding='utf-8')
 
         url=endpoint.format('/datos')
